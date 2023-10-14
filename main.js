6d21a721eb48f8b056bc62494585a46aae29f96a
Dithx <miuing11@gmail.com>
2023-10-15 06:05:10 +0700

    ready

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const menu = document.getElementById('menu');
 const navbar = document.getElementById('navbar');
 const account = document.getElementsByClassName('account');
 const checkbox = document.getElementById('checkbox');
+const body = document.getElementById('body');
 
 let count = 0;
 
@@ -12,12 +13,13 @@ checkbox.addEventListener('change', function(){
 
 function show() {
         if (count === 1) {
-            menu.classList.add('hidden'); // Hapus kelas 'hidden' untuk menampilkan menu
+            menu.classList.add('showMenu'); // Menambahkan class showMenu agar menu dapat terlihat
+            body.style.overflowY = 'hidden'; // Agar tidak bisa discroll pada saat menu navbar dibuka
             // navbar.style.display = "none";
             console.log(count);
         } else if (count === 2) {
-            menu.classList.remove('hidden'); // Tambahkan kelas 'hidden' untuk menyembunyikan menu
-            // navbar.style.display = "block";
+            menu.classList.remove('showMenu'); // Menghapus class showMenu
+            body.style.overflowY = ''; // agar bisa discroll kembali
             console.log(count);
             count = 0; // Reset count
         }
